697a3880b608dbbf7575476c521c0eae2b6b04e3
nicoleTeng <71899990@qq.com>
2018-06-18 22:46:20 +0800

    Add recyclerView demo

--- app/src/main/java/com/example/recyclerview/MyAdapter.java
+++ app/src/main/java/com/example/recyclerview/MyAdapter.java
@@ -2,6 +2,7 @@ package com.example.recyclerview;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,29 +12,92 @@ import com.example.nicole.R;
 
 import java.util.List;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private Context mContext;
-    private List<String> mList;
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> implements View.OnClickListener {
+    private static String TAG = "MyAdapter";
+    private Context context;
+    private List<String> list;
+    private OnChildClickListener listener;
+    private RecyclerView recyclerView;
 
     public MyAdapter(Context context, List<String> list) {
-        mContext = context;
-        mList = list;
+        this.context = context;
+        this.list = list;
+    }
+
+    public void setOnChildClickListener(OnChildClickListener listener) {
+        this.listener = listener;
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.recycler_item, parent, false);
+        Log.v(TAG, "txh onCreateViewHolder");
+        View view = null;
+        if (viewType == 0) {
+            view = LayoutInflater.from(context).inflate(R.layout.recycler_item_header, parent, false);
+        } else {
+            view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
+        }
+        view.setOnClickListener(this);
         return new MyViewHolder(view);
     }
 
     @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        this.recyclerView = recyclerView;
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+        this.recyclerView = null;
+    }
+
+    @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.textView.setText(mList.get(position));
+        Log.v(TAG, "txh onBindViewHolder, position = " + position);
+        holder.textView.setText(list.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return mList.size();
+        Log.v(TAG, "txh getItemCount = " + list.size());
+        return list.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return 0;
+        } else {
+            //return super.getItemViewType(position);
+            return 1;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (recyclerView != null && listener != null) {
+            int position = recyclerView.getChildAdapterPosition(view);
+            listener.onChildClick(recyclerView, view, position, list.get(position));
+        }
+    }
+
+    public void remove(int position) {
+        list.remove(position);
+//        notifyDataSetChanged();
+        notifyItemRemoved(position);
+    }
+
+    public void add(int position, String data) {
+        list.add(position, data);
+        notifyItemInserted(position);
+    }
+
+    public void change(int position, String data) {
+        list.remove(position);
+        list.add(position, data);
+        notifyItemChanged(position);
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -43,5 +107,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.textView_id);
         }
+    }
+
+    interface OnChildClickListener {
+        void onChildClick(RecyclerView recyclerView, View view, int Position, String data);
     }
 }
--- app/src/main/java/com/example/recyclerview/RecyclerActivity.java
+++ app/src/main/java/com/example/recyclerview/RecyclerActivity.java
@@ -1,9 +1,19 @@
 package com.example.recyclerview;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.nicole.R;
 
@@ -11,7 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class RecyclerActivity extends Activity {
+public class RecyclerActivity extends Activity implements MyAdapter.OnChildClickListener {
 
     private MyAdapter mAdapter;
 
@@ -19,12 +29,77 @@ public class RecyclerActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_id);
         List<String> list = new ArrayList<>();
-        for (int i = 0; i < 1009; i++) {
-            list.add(String.format(Locale.CHINA, "第%03d条数据", i));
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_id);
+
+        for (int i = 0; i < 100; i++) {
+            list.add(String.format(Locale.CHINA, "第%03d条数据%s", i, i % 2 == 0 ? "" : ""));
         }
+
         mAdapter = new MyAdapter(this, list);
+        mAdapter.setOnChildClickListener(this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
+                this, LinearLayoutManager.VERTICAL, false);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(
+                this, 3);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position == 0) {
+                    return 3;
+                }
+                if (position == 1) {
+                    return 2;
+                }
+                return 1;
+            }
+        });
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(
+                3, StaggeredGridLayoutManager.VERTICAL);
+
+        MyItemAnimator animator = new MyItemAnimator();
+        animator.setSupportsChangeAnimations(true);
+        animator.setRemoveDuration(3000);
+        animator.setMoveDuration(3000);
+        animator.setAddDuration(3000);
+        animator.setChangeDuration(3000);
+        recyclerView.setItemAnimator(animator);
+        recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(mAdapter);
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                super.onDraw(c, parent, state);
+                c.drawColor(Color.BLACK);
+            }
+
+            @Override
+            public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                super.onDrawOver(c, parent, state);
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+                c.drawBitmap(bitmap, c.getWidth() / 2 - bitmap.getWidth() /2,
+                        c.getHeight() / 2 - bitmap.getHeight() / 2, null);
+            }
+
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                int position = parent.getChildAdapterPosition(view);
+                if (position == 0) {
+                    outRect.set(0,20,0,20);
+                } else {
+                    outRect.set(0, 0, 0, 3);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onChildClick(RecyclerView recyclerView, View view, int position, String data) {
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+        mAdapter.remove(position);
+        //mAdapter.add(position, "新增数据");
+        //mAdapter.change(position, "新增数据");
     }
 }
