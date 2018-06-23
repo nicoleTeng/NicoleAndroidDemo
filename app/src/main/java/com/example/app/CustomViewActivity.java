b6d77115844fb30765052a7bb1bcc3156d36e97e
nicoleTeng <71899990@qq.com>
2018-06-23 11:36:15 +0800

    update custo view 2

--- app/src/main/java/com/example/app/CustomViewActivity.java
+++ app/src/main/java/com/example/app/CustomViewActivity.java
@@ -1,6 +1,15 @@
 package com.example.app;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.nicole.R;
 
 public class CustomViewActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_customview);
+    }
 }
